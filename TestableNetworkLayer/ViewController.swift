af846eccdfb4e64063c8a43cade05bea1b3edae0
Muhammed Celal Tok <celal.tok@hepsiburada.com>
2022-08-02 19:04:45 +0300

    Initial Commit

---
+++ TestableNetworkLayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestableNetworkLayer
+//
+//  Created by Muhammed Celal Tok on 2.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
